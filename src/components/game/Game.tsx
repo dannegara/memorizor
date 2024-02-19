c9c9fd74f3ebebd4f63040f0f89f7e266ffb610c
dannegara <dandannegara@MacBook-Air-Dan.lan>
2024-02-19 22:53:42 +0200

    Minor improvement

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -7,19 +7,16 @@ import { useGame } from "@hooks/game";
 const Game = () => {
   const { gameStep } = useGame();
 
-  if (gameStep === GameStep.NUMBER_OF_PLAYERS) {
-    return <NumberOfPlayersForm />;
+  switch (gameStep) {
+    case GameStep.NUMBER_OF_PLAYERS:
+      return <NumberOfPlayersForm />;
+    case GameStep.CARD_BOARD_GAME:
+      return <CardBoard />;
+    case GameStep.WINNER:
+      return <Winner />;
+    default:
+      throw new Error("Unexisting game step was set");
   }
-
-  if (gameStep === GameStep.CARD_BOARD_GAME) {
-    return <CardBoard />;
-  }
-
-  if (gameStep === GameStep.WINNER) {
-    return <Winner />;
-  }
-
-  throw new Error("Unexisting game step was set");
 };
 
 export default Game;
